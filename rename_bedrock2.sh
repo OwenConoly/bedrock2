52d6d6d7ec99d29d86cd27bd10fee72adcc673e3
Owen Conoly <otconoly@hotmail.com>
2024-06-29 23:14:38 -0400

    actually fix script

--- rename_bedrock2.sh
+++ rename_bedrock2.sh
@@ -1,2 +1,9 @@
+find . -type f -name Makefile -print0 | xargs -0 sed -i "s/-Q\s\([^ ]*\)\s$1/-Q \1 $2.$1/g" "${@:3}"
+find . -type f -name Makefile -print0 | xargs -0 sed -i "s/-R\s\([^ ]*\)\s$1/-R \1 $2.$1/g" "${@:3}"
+echo "s/From $1\(.*\) Require/From $2.$1\1 Require/g" >> rename_bedrock2.txt
+echo ":A; s/Import\(.*\)\s$1\./Import\1 $2.$1./g; tA" >> rename_bedrock2.txt
+echo ":B; s/Export\(.*\)\s$1\./Export\1 $2.$1./g; tB" >> rename_bedrock2.txt
+echo ":C; s/Require\(.*\)\s$1\./Require\1 $2.$1./g; tC" >> rename_bedrock2.txt
 ./refactor_sed.sh -f rename_bedrock2.txt
-find . -type f -name Makefile -print0 | xargs -0 sed -i "s/-Q \([^\s].*\) bedrock2/-Q \1 bedrock2A.bedrock2/g" "$@"
+rm rename_bedrock2.txt
+
