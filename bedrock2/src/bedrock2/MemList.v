798bdd3eccb98b7d19e06bc5c6263a4644cabb86
Owen Conoly <otconoly@hotmail.com>
2025-01-31 19:31:06 -0500

    better listmap representation

--- bedrock2/src/bedrock2/MemList.v
+++ bedrock2/src/bedrock2/MemList.v
@@ -1,41 +1,15 @@
-Require Import coqutil.Map.Interface coqutil.Lift1Prop.
-Require Import Coq.Lists.List.
-Import ListNotations map.
+Require Import coqutil.Map.Interface coqutil.Lift1Prop. Import map.
 
 Section Sep.
-  Context {key value} {map : map key value}.
-
-  (*Inductive disj_same_length : list map -> list map -> Prop :=
-  | disj_nil : disj_same_length [] []
-  | disj_cons a1 a2 m1 m2 : map.disjoint a1 a2 ->
-                            disj_same_length m1 m2 ->
-                            disj_same_length (a1 :: m1) (a2 :: m2).
-
-  Definition disj_length_leq (m1 m2 : list map) :=
-    (exists m0 m1', m1 = m0 ++ m1' /\ disj_same_length m1' m2).
-
-  (*this is obnoxious*)
-  Definition disj (m1 m2 : list map) :=
-    disj_length_leq m1 m2 \/ disj_length_leq m2 m1.
-
-  Definition putmany_list (m1 m2 : list map) :=
-    match m1, m2 with
-    | a1 :: m1', a2 :: m2' => map.putmany a1 a2 :: putmany_list m1' (*agh no they need to be reversed*)*)
-
-  Definition mem := nat -> map.
-  Definition equiv (m1 m2 : mem) := forall n, m1 n = m2 n.
-  Definition disj (m1 m2 : mem) := forall n, map.disjoint (m1 n) (m2 n).
-  Definition putmany_tower (m1 m2 : mem) := fun n => map.putmany (m1 n) (m2 n).
-  Definition split (m m1 m2 : mem) := forall n, map.split (m n) (m1 n) (m2 n).
-
-  Definition emp (P : Prop) := fun m : mem => equiv m (fun _ => map.empty) /\ P.
-  Definition sep (p q : mem -> Prop) m :=
+  Context {key value} {map : map (key * nat) value}.
+  Definition emp (P : Prop) := fun m : map => m = empty /\ P.
+  Definition sep (p q : map -> Prop) m :=
     exists mp mq, split m mp mq /\ p mp /\ q mq.
-  Definition ptsto k v := fun m : mem => exists n, m n = map.put empty k v.
+  Definition ptsto k v := fun m : map => exists n, m = put empty (k, n) v.
   (*what is this*)
-  (*Definition read k (P : value -> rep -> Prop) := (ex1 (fun v => sep (ptsto k v) (P v))).*)
+  (* Definition read k (P : value -> rep -> Prop) := (ex1 (fun v => sep (ptsto k v) (P v))). *)
 
-  Fixpoint seps (xs : list (mem -> Prop)) : mem -> Prop :=
+  Fixpoint seps (xs : list (rep -> Prop)) : rep -> Prop :=
     match xs with
     | cons x nil => x
     | cons x xs => sep x (seps xs)
@@ -43,9 +17,119 @@ Section Sep.
     end.
 End Sep.
 
-(*Declare Scope sep_scope.
+Declare Scope sep_scope.
 Delimit Scope sep_scope with sep.
 Infix "*" := sep (at level 40, left associativity) : sep_scope.
 Infix "⋆" := sep (at level 40, left associativity) : sep_scope.
 Notation "m =* P" := ((P%sep) m) (at level 70, only parsing).
-Notation "m =*> P" := (exists R, (sep P R) m) (at level 70, only parsing).*)
+Notation "m =*> P" := (exists R, (sep P R) m) (at level 70, only parsing).
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+(* Require Import coqutil.Map.Interface coqutil.Lift1Prop. *)
+(* Require Import Coq.Lists.List. *)
+(* Import ListNotations map. *)
+
+(* Section WithListmap. *)
+(*   Context {key value} {map : map key value}. *)
+  
+(*   Definition disj (m1 m2 : listmap) := forall n k v1 v2, *)
+(*     get m1 n k = Some v1 -> get m2 n k = Some v2 -> False. *)
+  
+
+(*   (*head of list is zeroth level, etc.  in particular, head1 and head2 are aligned.*) *)
+(*   Print putmany. *)
+  
+(*   Definition  *)
+
+(*   (* Fixpoint putmany_list (len : nat) (m1 m2 : list map) := *) *)
+(*   (* match len, m1, m2 with *) *)
+(*   (* | S len', a1 :: m1', a2 :: m2' => map.putmany a1 a2 :: putmany_list len' m1' m2' *) *)
+(*   (* | S len', [], a2 :: m2' => a2 :: putmany_list len' [] m2' *) *)
+(*   (* | S len', a1 :: m1', [] => a1 :: putmany_list len' m1' [] *) *)
+(*   (* | S len', [], [] => empty :: putmany_list len' [] [] *) *)
+(*   (* | O, _, _ => [] *) *)
+(*   (* end. *) Print map.map. *)
+
+(*   Definition split (m m1 m2 : list map) := equiv m (putmany_list m1 m2) /\ disj m1 m2. *)
+
+(*   Definition emp (P : Prop) := fun m => equiv m []. *)
+(*   Definition sep (p q : list map -> Prop) m := *)
+(*     exists mp mq, split m mp mq /\ p mp /\ q mq. *)
+(*   Definition ptsto k v := fun m : list map => exists n, equiv m (repeat map.empty n ++ [map.put empty k v]). *)
+
+(*   (*what is this*) *)
+(*   (*Definition read k (P : value -> rep -> Prop) := (ex1 (fun v => sep (ptsto k v) (P v))).*) *)
+
+(*   Fixpoint seps (xs : list (mem -> Prop)) : mem -> Prop := *)
+(*     match xs with *)
+(*     | cons x nil => x *)
+(*     | cons x xs => sep x (seps xs) *)
+(*     | nil => emp True *)
+(*     end. *)
+  
+(*   (*Definition mem := nat -> map. *)
+(*   Definition equiv (m1 m2 : mem) := forall n, m1 n = m2 n. *)
+(*   Definition disj (m1 m2 : mem) := forall n, map.disjoint (m1 n) (m2 n). *)
+(*   Definition putmany_tower (m1 m2 : mem) := fun n => map.putmany (m1 n) (m2 n). *)
+(*   Definition split (m m1 m2 : mem) := forall n, map.split (m n) (m1 n) (m2 n). *)
+
+(*   Definition emp (P : Prop) := fun m : mem => equiv m (fun _ => map.empty) /\ P. *)
+(*   Definition sep (p q : mem -> Prop) m := *)
+(*     exists mp mq, split m mp mq /\ p mp /\ q mq. *)
+(*   Definition ptsto k v := fun m : mem => exists n, m n = map.put empty k v. *)
+(*   (*what is this*) *)
+(*   (*Definition read k (P : value -> rep -> Prop) := (ex1 (fun v => sep (ptsto k v) (P v))).*) *)
+
+(*   Fixpoint seps (xs : list (mem -> Prop)) : mem -> Prop := *)
+(*     match xs with *)
+(*     | cons x nil => x *)
+(*     | cons x xs => sep x (seps xs) *)
+(*     | nil => emp True *)
+(*     end.*) *)
+
+  
+(* End Sep. *)
+
+(* (*Declare Scope sep_scope. *)
+(* Delimit Scope sep_scope with sep. *)
+(* Infix "*" := sep (at level 40, left associativity) : sep_scope. *)
+(* Infix "⋆" := sep (at level 40, left associativity) : sep_scope. *)
+(* Notation "m =* P" := ((P%sep) m) (at level 70, only parsing). *)
+(* Notation "m =*> P" := (exists R, (sep P R) m) (at level 70, only parsing).*) *)
